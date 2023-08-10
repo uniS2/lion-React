bc6001eedb629a39b2ecb68a291d637e41f78c9f
uniS2 <s2oie1010@gmail.com>
2023-08-10 10:35:05 +0900

    add. 4일차 수업 - export, exprt default 차이점 해설 - utils 모듈 - getRandom, getRandomMinMax - currency, cyrrencyKR - numberWithComma

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import RootLayout from "./layout/RootLayout";
-import Home from "./pages/Home";
-// import About from "./pages/About";
+import RootLayout from './layout/RootLayout';
+import Practice from './pages/Practice';
 
-//- {/* 홈 페이지 : index.html */}
-//- {/* 소개 페이지 : about.html */}
-//- {/* 제품 목록 페이지 : products.html */}
-//- {/* 의뢰 페이지 : contact.html */}
-
-// App 함수 컴포넌트를 작성합니다.
 function App() {
-  // JSX 값 반환
   return (
     <div className="App">
       <RootLayout>
-        <Home />
-        {/* <About /> */}
+        <Practice />
       </RootLayout>
     </div>
   );
 }
 
 export default App;
-
-{/* props
-<RootLayout>
-  <b>페이지의</b>
-  <span>주요</span>
-  <em>콘텐츠</em>
-</RootLayout>
-*/}
