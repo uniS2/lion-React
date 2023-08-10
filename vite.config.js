0a19d1a552a0a6602c216306c26166890d58017c
uniS2 <s2oie1010@gmail.com>
2023-08-11 01:08:12 +0900

    add. 4일차 수업 - Vite 절대 경로 구성
    - resolve.alias
    - rollup alias plugin
    - Node.js path module

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'  // Node.js 런타임이 기본 제공하는 모듈 (파일 경로). from path 사용가능
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    // alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
