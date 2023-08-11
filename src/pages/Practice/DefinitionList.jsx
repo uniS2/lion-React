16bdfa431cd2a34b76c268a8106206cce08e6b98
uniS2 <s2oie1010@gmail.com>
2023-08-11 12:12:11 +0900

    add. 5일차 오전 수업 - 컴포넌트 추출 및 리스트 렌더링
    [컴포넌트 추출]
    - Controller, GoToButton
    
    [리스트 렌더링(역순)]
    - 외부에서 props 전달된 함수 결과를 활용
    - div (또는 React.Fragment)

--- src/pages/Practice/DefinitionList.jsx
+++ src/pages/Practice/DefinitionList.jsx
@@ -4,47 +4,18 @@ import ConditionalDisplay from "./parts/ConditionalDisplay";
 import ConditionalRendering from "./parts/ConditionalRendering";
 import DisplayingData from "./parts/DisplayingData";
 import RenderingLists from "./parts/RenderingLists";
+import { imageType, isShowReactImage, statusMessage } from './parts/data';
 
-/* 데이터 ---------------------------------------------------------------------- */
-
-let imageType = "react"; // 'react' | 'vite'
-
-let isShowReactImage = true;
-
-const statusMessage = [
-  "⌛️ 대기", // pending
-  "⏳ 로딩 중...", // loading
-  "✅ 로딩 성공!", // success (resolve)
-  "❌ 로딩 실패.", // error (fail, reject)
-];
-
-const reactLibrary = {
-  name: "React",
-  author: "조던 워케(Jordan Walke)",
-  writtenIn: "JavaScript",
-  type: "JavaScript 라이브러리",
-  license: "MIT",
-};
-
-/* 컴포넌트 --------------------------------------------------------------------- */
 
 function DefinitionList() {
   // 배열 데이터 → 순환 → 새롭게 가공된 배열 반환하고자 한다면?
   // 어떻게 해야 할까요?
-  const renderList = () => {
-    const renderListItem = (message) => <li key={message}>{message}</li>;
-
-    return statusMessage.map(renderListItem);
-
-    // return [
-    //   <li key={statusMessage[0]}>{statusMessage[0]}</li>,
-    //   <li key={statusMessage[1]}>{statusMessage[1]}</li>,
-    //   <li key={statusMessage[2]}>{statusMessage[2]}</li>,
-    //   <li key={statusMessage[3]}>{statusMessage[3]}</li>,
-    // ];
+  const renderList = ({reverse = false }) => {
+    const renderListItem = (message) => <li key={message}>{message}</li>
+    return (!reverse ? statusMessage : statusMessage.toReversed().map(renderListItem))
   };
 
-  const allHidden = false;
+  const allHidden = true;
 
   return (
     <dl className="descriptionList">
--- src/pages/Practice/parts/RenderingLists.jsx
+++ src/pages/Practice/parts/RenderingLists.jsx
@@ -1,10 +1,13 @@
+import { reactLibrary } from "./data";
+
 function RenderingLists({ statusMessage, renderList }) {
+
   return (
     <>
       <dt>리스트 렌더링(list rendering)</dt>
       <dd>
         <p>상태 메시지(status message) 배열을 리스트 렌더링합니다.</p>
-        {/* <ul className="renderList">{renderList()}</ul> */}
+        {/* <ul className="renderList">{renderList({ reverse: false })}</ul> */}
         <ul className="renderList">
           {statusMessage.map((message) => {
             return <li key={message}>{message}</li>;
@@ -22,6 +25,18 @@ function RenderingLists({ statusMessage, renderList }) {
         </p>
         <dl className="reactLibrary">
           {/* 여기서 설명 목록으로 리스트 렌더링 합니다. */}
+          {/* reactLibrary 객체를 순환해서 리스트 렌더링 */}
+          {/* <pre>{JSON.stringify(reactLibrary, null, 2)}</pre> */}
+          {/* <pre>{JSON.stringify(Object.entries(reactLibrary), null, 2)}</pre> */}
+
+          {Object.entries(reactLibrary).map(([key, value]) => {
+            return (
+              <div key={key}>
+                <dt>{key}</dt>
+                <dd>{value}</dd>
+              </div>
+            );
+          })}
         </dl>
       </dd>
     </>
