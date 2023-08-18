72cf61c5410e82d537042f488ad6ac3ad3838a71
uniS2 <s2oie1010@gmail.com>
2023-08-18 12:29:28 +0900

    edit. vite.config.js 수정
    - 개발 환경 여부 확인

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'  // Node.js 런타임이 기본 제공하는 모듈 (파일 경로). from path 사용가능
 
-
+// 개발 환경인지 여부를 나타내는 불리언 값
+const isDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   css: {
     devSourcemap: true,
     modules: {
-      generateScopedName : `[name]_[local]_[hase:base64:9]`
+      generateScopedName : isDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
     }
   },
   resolve: {
